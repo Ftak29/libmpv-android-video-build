798932de81fd8cc7308277737c82d560d4c378b3
Ftak29 <ftak29@outlook.com>
2026-03-13 10:02:02 -0400

    Update libx264.sh

--- buildscripts/scripts/libx264.sh
+++ buildscripts/scripts/libx264.sh
@@ -8,19 +8,41 @@ build=_build$ndk_suffix
 if [ "$1" == "build" ]; then
 	true
 elif [ "$1" == "clean" ]; then
-	rm -rf $build
+	rm -rf "$build"
 	exit 0
 else
 	exit 255
 fi
 
-cp ../../scripts/libx264.build meson.build
+mkdir -p "$build"
+cd "$build"
 
-unset CC CXX # meson wants these unset
+case "${CC%%-*}" in
+    armv7a)  x264_host=arm-linux ;;
+    aarch64) x264_host=aarch64-linux-android ;;
+    i686)    x264_host=i686-linux-android ;;
+    x86_64)  x264_host=x86_64-linux-android ;;
+    *)
+        echo "Unsupported x264 host for compiler: $CC" >&2
+        exit 1
+        ;;
+esac
 
-mkdir $build
+sysroot="$($CC --print-sysroot)"
 
-meson setup $build --cross-file "$prefix_dir"/crossfile.txt --prefix="$prefix_dir"
+../configure \
+    --prefix="$prefix_dir" \
+    --host="$x264_host" \
+    --sysroot="$sysroot" \
+    --disable-cli \
+    --enable-static \
+    --enable-pic \
+    CC="$CC" \
+    CXX="$CXX" \
+    AR="$AR" \
+    AS="$AS" \
+    RANLIB="$RANLIB" \
+    STRIP="${STRIP:-llvm-strip}"
 
-meson compile -C $build libx264
-meson install -C $build
+make -j"$cores"
+make install
