96e548f1082f38b74445001112e69ac901172e45
Ftak29 <ftak29@outlook.com>
2026-04-13 21:20:55 -0400

    Create libzvbi.sh

---
+++ buildscripts/scripts/libzvbi.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -e
+
+. ../../include/depinfo.sh
+. ../../include/path.sh
+
+if [ "$1" == "build" ]; then
+	true
+elif [ "$1" == "clean" ]; then
+	rm -rf _build$ndk_suffix
+	exit 0
+else
+	exit 255
+fi
+
+[ -f configure ] || ./autogen.sh
+
+mkdir -p _build$ndk_suffix
+cd _build$ndk_suffix
+
+../configure \
+	CC="$CC" \
+	CXX="$CXX" \
+	AR="$AR" \
+	RANLIB="$RANLIB" \
+	CFLAGS="-fPIC" \
+	CXXFLAGS="-fPIC" \
+	LDFLAGS="$LDFLAGS" \
+	--host="$ndk_triple" \
+	--prefix=/ \
+	--with-pic \
+	--enable-static \
+	--disable-shared \
+	--without-doxygen \
+	--without-x \
+	--disable-dvb \
+	--disable-bktr \
+	--disable-nls \
+	--disable-proxy \
+	--disable-examples \
+	--disable-tests
+
+make -j$cores
+make DESTDIR="$prefix_dir" install
