f36fe62331aca28a84e46ca36881548f23296aa7
Ftak29 <ftak29@outlook.com>
2026-04-14 21:38:19 -0400

    Update libzvbi.sh

--- buildscripts/scripts/libzvbi.sh
+++ buildscripts/scripts/libzvbi.sh
@@ -7,51 +7,57 @@ if [ "$1" == "build" ]; then
 	true
 elif [ "$1" == "clean" ]; then
 	rm -rf _build$ndk_suffix
+	rm -f config.cache
+	rm -f ../config.cache
 	exit 0
 else
 	exit 255
 fi
 
+# Patch configure.ac before autogen.sh regenerates configure.
+python3 - <<'PY'
+from pathlib import Path
+
+p = Path("configure.ac")
+s = p.read_text()
+
+old = """AC_CHECK_LIB([pthread], [pthread_create],,[
+  AC_CHECK_LIB([pthreadGC2], [pthread_create],,[
+    AC_LINK_IFELSE([AC_LANG_PROGRAM([[]], [[pthread_create();]])],[],[
+      dnl same as previous, but use '-pthread' instead of '-lpthread'
+      LDFLAGS="$LDFLAGS -pthread"
+      AC_LINK_IFELSE([AC_LANG_PROGRAM([[]], [[pthread_create();]])],[],[
+        AC_MSG_ERROR([Unable to link pthread functions])
+      ])
+    ])
+  ])
+])"""
+
+new = """AC_CHECK_LIB([pthread], [pthread_create],,[
+  AC_CHECK_LIB([pthreadGC2], [pthread_create],,[
+    AC_LINK_IFELSE([AC_LANG_PROGRAM([[#include <pthread.h>
+static void *zvbi_android_pthread_stub(void *arg) { return arg; }]], [[pthread_t t; pthread_create(&t, 0, zvbi_android_pthread_stub, 0);]])],[],[
+      dnl same as previous, but use '-pthread' instead of '-lpthread'
+      save_LDFLAGS="$LDFLAGS"
+      LDFLAGS="$LDFLAGS -pthread"
+      AC_LINK_IFELSE([AC_LANG_PROGRAM([[#include <pthread.h>
+static void *zvbi_android_pthread_stub(void *arg) { return arg; }]], [[pthread_t t; pthread_create(&t, 0, zvbi_android_pthread_stub, 0);]])],[],[
+        AC_MSG_ERROR([Unable to link pthread functions])
+      ])
+      LDFLAGS="$save_LDFLAGS"
+    ])
+  ])
+])"""
+
+if old not in s:
+    raise SystemExit("pthread block not found in configure.ac")
+
+p.write_text(s.replace(old, new, 1))
+print("Patched configure.ac pthread block")
+PY
+
 if [ ! -f configure ]; then
 	command -v autopoint >/dev/null 2>&1 || { echo "autopoint not found; install gettext/autopoint"; exit 1; }
-	python3 - <<'PY'
-	from pathlib import Path
-	p = Path("configure.ac")
-	s = p.read_text()
-	
-	old = """AC_CHECK_LIB([pthread], [pthread_create],,[
-	  AC_CHECK_LIB([pthreadGC2], [pthread_create],,[
-	    AC_LINK_IFELSE([AC_LANG_PROGRAM([[]], [[pthread_create();]])],[],[
-	      dnl same as previous, but use '-pthread' instead of '-lpthread'
-	      LDFLAGS="$LDFLAGS -pthread"
-	      AC_LINK_IFELSE([AC_LANG_PROGRAM([[]], [[pthread_create();]])],[],[
-	        AC_MSG_ERROR([Unable to link pthread functions])
-	      ])
-	    ])
-	  ])
-	])"""
-	
-	new = """AC_CHECK_LIB([pthread], [pthread_create],,[
-	  AC_CHECK_LIB([pthreadGC2], [pthread_create],,[
-	    AC_LINK_IFELSE([AC_LANG_PROGRAM([[#include <pthread.h>
-	static void *zvbi_android_pthread_stub(void *arg) { return arg; }]], [[pthread_t t; pthread_create(&t, 0, zvbi_android_pthread_stub, 0);]])],[],[
-	      dnl same as previous, but use '-pthread' instead of '-lpthread'
-	      save_LDFLAGS="$LDFLAGS"
-	      LDFLAGS="$LDFLAGS -pthread"
-	      AC_LINK_IFELSE([AC_LANG_PROGRAM([[#include <pthread.h>
-	static void *zvbi_android_pthread_stub(void *arg) { return arg; }]], [[pthread_t t; pthread_create(&t, 0, zvbi_android_pthread_stub, 0);]])],[],[
-	        AC_MSG_ERROR([Unable to link pthread functions])
-	      ])
-	      LDFLAGS="$save_LDFLAGS"
-	    ])
-	  ])
-	])"""
-	
-	if old not in s:
-	    raise SystemExit("pthread block not found in configure.ac")
-	p.write_text(s.replace(old, new, 1))
-	print("Patched configure.ac pthread block")
-	PY	
 	./autogen.sh
 fi
 
@@ -62,32 +68,32 @@ rm -f config.cache
 rm -f ../config.cache
 
 env \
-  ac_cv_func_pthread_create=yes \
-  ac_cv_search_pthread_create='none required' \
-  ac_cv_lib_pthread_pthread_create=no \
-  ac_cv_lib_pthreadGC2_pthread_create=no \
-  ../configure \
-    CC="$CC" \
-    CXX="$CXX" \
-    AR="$AR" \
-    RANLIB="$RANLIB" \
-    CFLAGS="$CFLAGS -fPIC -pthread" \
-    CXXFLAGS="$CXXFLAGS -fPIC -pthread" \
-    LDFLAGS="$LDFLAGS -pthread" \
-    LIBS="-lc" \
-    --host="$ndk_triple" \
-    --prefix=/ \
-    --with-pic \
-    --enable-static \
-    --disable-shared \
-    --without-doxygen \
-    --without-x \
-    --disable-dvb \
-    --disable-bktr \
-    --disable-nls \
-    --disable-proxy \
-    --disable-examples \
-    --disable-tests
-	
+	ac_cv_func_pthread_create=yes \
+	ac_cv_search_pthread_create='none required' \
+	ac_cv_lib_pthread_pthread_create=no \
+	ac_cv_lib_pthreadGC2_pthread_create=no \
+	../configure \
+	CC="$CC" \
+	CXX="$CXX" \
+	AR="$AR" \
+	RANLIB="$RANLIB" \
+	CFLAGS="$CFLAGS -fPIC -pthread" \
+	CXXFLAGS="$CXXFLAGS -fPIC -pthread" \
+	LDFLAGS="$LDFLAGS -pthread" \
+	LIBS="-lc" \
+	--host="$ndk_triple" \
+	--prefix=/ \
+	--with-pic \
+	--enable-static \
+	--disable-shared \
+	--without-doxygen \
+	--without-x \
+	--disable-dvb \
+	--disable-bktr \
+	--disable-nls \
+	--disable-proxy \
+	--disable-examples \
+	--disable-tests
+
 make -j$cores
 make DESTDIR="$prefix_dir" install
